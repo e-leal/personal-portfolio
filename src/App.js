04e8372b730629755e8e1f164cfe0fd9b2545ee4
e-leal <leal.elyssa03@gmail.com>
2020-10-04 12:15:34 -0500

    Added reference in app.js to about page component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
+import Project from  './components/Project';
+import About from './components/pages/About';
 import './App.css';
 
 function App() {
@@ -7,17 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <About />
       </header>
     </div>
   );
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+    <div>
+        <h1>About Me</h1>
+        <p>
+            Pariatur incididunt enim sit anim eiusmod esse id cillum. Exercitation laboris nisi ad nostrud quis aute duis consequat aute eu. Fugiat id aliqua culpa sint aliqua ad cillum excepteur. Aute ad consectetur pariatur dolor duis incididunt.
+        </p>
+    </div>
+);
+
+export default About;
