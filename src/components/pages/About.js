97d277ed8272e16100f95dfc228088df20f55872
e-leal <leal.elyssa03@gmail.com>
2021-03-23 22:28:49 -0500

    remove tabbed space at beginning of professional summary

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,7 @@ const About = () => (
     <Container>
         <h1>About Me</h1>
         <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;I'm an enthusiastic system engineer eager to contribute to team success through hard work, attention to detail and excellent organizational and communication skills. Clear understanding of changepoint management and error troubleshooting and training in web development technologies. Motivated to learn, grow and excel in system development.
+        I'm an enthusiastic system engineer eager to contribute to team success through hard work, attention to detail and excellent organizational and communication skills. Clear understanding of changepoint management and error troubleshooting and training in web development technologies. Motivated to learn, grow and excel in system development.
         </p>
     </Container>
 );
