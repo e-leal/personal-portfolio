cd21440fea9d142abe9b09fd97260506a7dc2ac6
e-leal <leal.elyssa03@gmail.com>
2021-03-24 18:31:19 -0500

    Updates

---
+++ precache-manifest.e9562bd28e2742125cc8a706fce29010.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7e9e0061f0a69a3d62e443749a7baf",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "b3ba644fb5181b81e7ee",
+    "url": "/personal-portfolio/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "0249311358079d0cb321",
+    "url": "/personal-portfolio/static/css/main.f52c5d02.chunk.css"
+  },
+  {
+    "revision": "b3ba644fb5181b81e7ee",
+    "url": "/personal-portfolio/static/js/2.e6feea5d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/personal-portfolio/static/js/2.e6feea5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0249311358079d0cb321",
+    "url": "/personal-portfolio/static/js/main.3d328a79.chunk.js"
+  },
+  {
+    "revision": "1fd8320ed472344e7a3b",
+    "url": "/personal-portfolio/static/js/runtime-main.41dc66a3.js"
+  },
+  {
+    "revision": "cf52979937fb6faa6428d5adb382fa9e",
+    "url": "/personal-portfolio/static/media/Elyssa_Leal_Resume_21.cf529799.PDF"
+  },
+  {
+    "revision": "e4230fa0868dee46977d15b5b68ecb85",
+    "url": "/personal-portfolio/static/media/Horiseon_Screenshot2.e4230fa0.png"
+  },
+  {
+    "revision": "00a827cf48bb1a474e21a7dc9413d6a9",
+    "url": "/personal-portfolio/static/media/RedBag-Screenshot.00a827cf.png"
+  },
+  {
+    "revision": "41819bb44e59feb851547813679fc75f",
+    "url": "/personal-portfolio/static/media/RunBuddy-Screenshot.41819bb4.png"
+  },
+  {
+    "revision": "ffad124f5aa97c4ea979a6f7a9fa1514",
+    "url": "/personal-portfolio/static/media/WorthMyTime_Screenshot.ffad124f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "86a7a958dbd5a9efde0ca48c92b20440",
+    "url": "/personal-portfolio/static/media/profile_image.86a7a958.jpg"
+  }
+]);
