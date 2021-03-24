fc8e03c6f719ce65f791eb0b5de45a2a8de85494
e-leal <leal.elyssa03@gmail.com>
2021-03-23 23:13:19 -0500

    add headshot to about page

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css'
+import headshot from '../assets/profile_image.jpg'
+
+
+function Profile() {
+return (
+    
+            <img className="hero" src={headshot} alt="Profile headshot" />
+    
+);
+}
+
+export default Profile;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Card, Container} from 'react-bootstrap';
+import Profile from '../Profile';
 
 
 const About = () => (
     <Container>
         <h1>About Me</h1>
+        <Profile></Profile>
         <p>
         I'm an enthusiastic system engineer eager to contribute to team success through hard work, attention to detail and excellent organizational and communication skills. Clear understanding of changepoint management and error troubleshooting and training in web development technologies. Motivated to learn, grow and excel in system development.
         </p>
